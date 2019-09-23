54b540910a7a91923bb6a5125ea725bbd35312ff
Seba Siby <seba.siby@optus.com.au>
2019-09-23 15:29:25 +1000

    Converted to Microservice

--- src/main/java/au/com/optus/ctc/ClinicalTrialsConnectApp.java
+++ src/main/java/au/com/optus/ctc/ClinicalTrialsConnectApp.java
@@ -1,43 +1,29 @@
 package au.com.optus.ctc;
 
+import java.util.Collections;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-
-import java.util.Collections;
 
 /**
  * @author revathyms
  */
 @EnableWebMvc
 @EnableConfigurationProperties
-@EntityScan(basePackages = {"au.com.optus.ctc.model"})
+@EntityScan(basePackages = { "au.com.optus.ctc.model" })
 @EnableJpaAuditing
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class ClinicalTrialsConnectApp {
-    public static void main(String[] args) {
-        SpringApplication app = new SpringApplication(ClinicalTrialsConnectApp.class);
-        app.setDefaultProperties(Collections.singletonMap("server.port", "9090"));
-        app.run(args);
-    }
-
-    @EnableWebSecurity
-    public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-
-        @Override
-        protected void configure(HttpSecurity http) throws Exception {
-            http
-                    .authorizeRequests()
-                    .anyRequest().authenticated()
-                    .and()
-                    .httpBasic();
-            http.cors();
-        }
-    }
+	public static void main(String[] args) {
+		SpringApplication app = new SpringApplication(ClinicalTrialsConnectApp.class);
+		app.setDefaultProperties(Collections.singletonMap("server.port", "9090"));
+		app.run(args);
+	}
 }
+
+
