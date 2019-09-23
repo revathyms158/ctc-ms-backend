c700837f5d81070bd6dfce47118b7053bfad66a4
Revathy M S <revathy.ms@optus.com.au>
2019-09-23 17:18:10 +1000

    Added missing file config

--- src/main/java/au/com/optus/ctc/SecurityConfiguration.java
+++ src/main/java/au/com/optus/ctc/SecurityConfiguration.java
@@ -8,8 +8,16 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.csrf().disable().authorizeRequests().anyRequest().permitAll();
-	}
+
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http
+					.authorizeRequests()
+					.anyRequest().authenticated()
+					.and()
+					.csrf().disable()
+					.httpBasic();
+			http.cors();
+		}
+
 }
--- src/main/java/au/com/optus/ctc/controller/MyAccountController.java
+++ src/main/java/au/com/optus/ctc/controller/MyAccountController.java
@@ -3,6 +3,7 @@ package au.com.optus.ctc.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +18,7 @@ import au.com.optus.ctc.model.AccountProfile;
 /**
  * @author revathyms
  */
+@CrossOrigin(origins = {"http://localhost:4200"})
 @RestController
 @RequestMapping(value = "/api/ctc")
 public class MyAccountController {
