577a1f16035e7c4239d996df8df1d698d568b689
revathy <revathy@revathy-hp-probook-650-g1>
2019-11-11 20:21:41 +1100

    Updated response class

---
+++ src/main/java/au/com/optus/ctc/model/AccountProfileResponse.java
@@ -0,0 +1,14 @@
+package au.com.optus.ctc.model;
+
+public class AccountProfileResponse {
+	private String id;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+}
